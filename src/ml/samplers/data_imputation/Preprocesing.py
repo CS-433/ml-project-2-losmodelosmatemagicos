4948928c920829d08c8023440f6b452d62af09f7
AfroDeivid <92383275+AfroDeivid@users.noreply.github.com>
2023-11-27 10:59:55 +0100

    docsstring

--- src/ml/samplers/data_imputation/Preprocesing.py
+++ src/ml/samplers/data_imputation/Preprocesing.py
@@ -1,14 +1,59 @@
 import numpy as np
 
 class Preprocesing:
+    """
+    Class for preprocessing data: encoding and decoding.
+
+    Attributes
+    ----------
+    num_states : int
+        Number of states in the data.
+    num_actions : int
+        Number of actions in the data.
+    token_dict : dict
+        Dictionary mapping tokens to their corresponding indices.
+    sep : float, optional
+        Used to represent breaks if longer than this value , by default 0 (no breaks).
+
+    Methods
+    -------
+    __init__(self, num_states: int, num_actions: int, token_dict: dict, sep: float = 0) -> None:
+        Initializes the Preprocesing object with the given parameters.
+
+    encode_vocabulary(self, data: dict):
+        Encodes the vocabulary in the given data.
+
+    encode_sep(self, data, index_break = 8):
+        Encodes 
+
+    decode_vocabulary(self, data):
+        Decodes the vocabulary in the given data.
+    """
 
     def __init__(self, num_states: int, num_actions: int, token_dict: dict, sep: float = 0) -> None:
+        """
+        Initializes the Preprocesing object with the given parameters.
+        """
+
         self.ns = num_states
         self.na = num_actions
         self.token_dict = token_dict
         self.sep = sep
 
     def encode_vocabulary(self, data: dict):
+        """
+        Encodes the vocabulary in the given data.
+
+        Parameters
+        ----------
+        data : dict
+            Data to be encoded.
+
+        Returns
+        -------
+        dict
+            Encoded data.
+        """
 
         encoded_data = data.copy()
 
@@ -26,6 +71,16 @@ class Preprocesing:
         return encoded_data
     
     def encode_sep(self, data, index_break = 8):
+        """
+        Encodes the breaks if longer than this value "sep", in the given data.
+
+        Parameters
+        ----------
+        data : dict
+            Data to be encoded.
+        index_break : int, optional
+            Index break value, by default 8.
+        """
 
         for i in range(len(data['sequences'])):
                 for j in range(len(data['sequences'][i]['sequence'])):
@@ -33,6 +88,19 @@ class Preprocesing:
                         data['sequences'][i]['sequence'][j] = self.token_dict['[SEP]']
 
     def decode_vocabulary(self, data):
+        """
+        Decodes the vocabulary in the given data.
+
+        Parameters
+        ----------
+        data : dict
+            Data to be decoded.
+
+        Returns
+        -------
+        dict
+            Decoded data.
+        """
         
         for i in range(len(data['sequences'])):
             for j in range(len(data['sequences'][i]['sequence'])):
