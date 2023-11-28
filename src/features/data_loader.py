ed6d1eacc80cf5b8406d6f7b8251fd0c0668a441
Lugiasc <adrien.vauthey@epfl.ch>
2023-11-28 13:36:07 +0100

    using official path to the data

--- src/features/data_loader.py
+++ src/features/data_loader.py
@@ -19,7 +19,7 @@ class DataLoader:
         """
         ################################ CHEMLAB ################################
         ##### Features #####
-        path = '../data/ml4science_data.pkl'
+        path = '\\ic1files\D-VET\Projects\ChemLab\ml4science-data'
         with open(path, 'rb') as fp:
             full_data = pickle.load(fp)
         return full_data
