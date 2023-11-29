78b141b45338582f35cfe75d753d6668769025d2
YannickDetrois <yannick.detrois@outlook.de>
2023-11-29 14:59:45 +0100

    BERT is slowly rising

--- src/ml/samplers/data_imputation/BERT.py
+++ src/ml/samplers/data_imputation/BERT.py
@@ -1,20 +1,176 @@
-# TODO create of model based on BERT
-from transformers import BertTokenizer, BertForMaskedLM, pipeline
-import torch
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import TextVectorization
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+import glob
+import re
+from pprint import pprint
 
-# Scaled numerical data (as previously scaled)
-scaled_data = [1234, 567, 890, 2345, 123]
+@dataclass
+class Config:
+    MAX_LEN = 256
+    BATCH_SIZE = 32
+    LR = 0.001
+    VOCAB_SIZE = 30000
+    EMBED_DIM = 128
+    NUM_HEAD = 8  
+    FF_DIM = 128 
+    NUM_LAYERS = 1
 
-# Load BERT tokenizer and model
-model_name = 'bert-base-uncased'  # or specify other variations
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForMaskedLM.from_pretrained(model_name)
-nlp_fill = pipeline('fill-mask', model=model, tokenizer=tokenizer)
 
-# Convert scaled numerical data to token IDs
-token_ids = torch.tensor(scaled_data).unsqueeze(0)  # Assuming one batch of data
+def bert_module(config: Config, query, key, value, i):
+    # Multi headed self-attention
+    attention_output = layers.MultiHeadAttention(
+        num_heads=config.NUM_HEAD,
+        key_dim=config.EMBED_DIM // config.NUM_HEAD,
+        name="encoder_{}/multiheadattention".format(i),
+    )(query, key, value)
+    attention_output = layers.Dropout(0.1, name="encoder_{}/att_dropout".format(i))(
+        attention_output
+    )
+    attention_output = layers.LayerNormalization(
+        epsilon=1e-6, name="encoder_{}/att_layernormalization".format(i)
+    )(query + attention_output)
 
-# Generate synthetic text using BERT's masked language modeling
-outputs = model.generate(token_ids)
-synthetic_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print("Synthetic Text:", synthetic_text)
+    # Feed-forward layer
+    ffn = keras.Sequential(
+        [
+            layers.Dense(config.FF_DIM, activation="relu"),
+            layers.Dense(config.EMBED_DIM),
+        ],
+        name="encoder_{}/ffn".format(i),
+    )
+    ffn_output = ffn(attention_output)
+    ffn_output = layers.Dropout(0.1, name="encoder_{}/ffn_dropout".format(i))(
+        ffn_output
+    )
+    sequence_output = layers.LayerNormalization(
+        epsilon=1e-6, name="encoder_{}/ffn_layernormalization".format(i)
+    )(attention_output + ffn_output)
+    return sequence_output
+
+
+def get_pos_encoding_matrix(max_len, d_emb):
+    pos_enc = np.array(
+        [
+            [pos / np.power(10000, 2 * (j // 2) / d_emb) for j in range(d_emb)]
+            if pos != 0
+            else np.zeros(d_emb)
+            for pos in range(max_len)
+        ]
+    )
+    pos_enc[1:, 0::2] = np.sin(pos_enc[1:, 0::2])  # dim 2i
+    pos_enc[1:, 1::2] = np.cos(pos_enc[1:, 1::2])  # dim 2i+1
+    return pos_enc
+
+
+loss_fn = keras.losses.SparseCategoricalCrossentropy(
+    reduction=tf.keras.losses.Reduction.NONE
+)
+loss_tracker = tf.keras.metrics.Mean(name="loss")
+
+
+class MaskedLanguageModel(tf.keras.Model):
+    def train_step(self, inputs):
+        if len(inputs) == 3:
+            features, labels, sample_weight = inputs
+        else:
+            features, labels = inputs
+            sample_weight = None
+
+        with tf.GradientTape() as tape:
+            predictions = self(features, training=True)
+            loss = loss_fn(labels, predictions, sample_weight=sample_weight)
+
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+        # Compute our own metrics
+        loss_tracker.update_state(loss, sample_weight=sample_weight)
+
+        # Return a dict mapping metric names to current value
+        return {"loss": loss_tracker.result()}
+
+    @property
+    def metrics(self):
+        # We list our `Metric` objects here so that `reset_states()` can be
+        # called automatically at the start of each epoch
+        # or at the start of `evaluate()`.
+        # If you don't implement this property, you have to call
+        # `reset_states()` yourself at the time of your choosing.
+        return [loss_tracker]
+
+
+def create_masked_language_bert_model(config: Config):
+    inputs = layers.Input((config.MAX_LEN,), dtype=tf.int64)
+
+    word_embeddings = layers.Embedding(
+        config.VOCAB_SIZE, config.EMBED_DIM, name="word_embedding"
+    )(inputs)
+    position_embeddings = layers.Embedding(
+        input_dim=config.MAX_LEN,
+        output_dim=config.EMBED_DIM,
+        weights=[get_pos_encoding_matrix(config.MAX_LEN, config.EMBED_DIM)],
+        name="position_embedding",
+    )(tf.range(start=0, limit=config.MAX_LEN, delta=1))
+    embeddings = word_embeddings + position_embeddings
+
+    encoder_output = embeddings
+    for i in range(config.NUM_LAYERS):
+        encoder_output = bert_module(encoder_output, encoder_output, encoder_output, i)
+
+    mlm_output = layers.Dense(config.VOCAB_SIZE, name="mlm_cls", activation="softmax")(
+        encoder_output
+    )
+    mlm_model = MaskedLanguageModel(inputs, mlm_output, name="masked_bert_model")
+
+    optimizer = keras.optimizers.Adam(learning_rate=config.LR)
+    mlm_model.compile(optimizer=optimizer)
+    return mlm_model
+
+
+id2token = dict(enumerate(vectorize_layer.get_vocabulary()))
+token2id = {y: x for x, y in id2token.items()}
+
+
+class MaskedTextGenerator(keras.callbacks.Callback):
+    def __init__(self, sample_tokens, top_k=5):
+        self.sample_tokens = sample_tokens
+        self.k = top_k
+
+    def decode(self, tokens):
+        return " ".join([id2token[t] for t in tokens if t != 0])
+
+    def convert_ids_to_tokens(self, id):
+        return id2token[id]
+
+    def on_epoch_end(self, epoch, logs=None):
+        prediction = self.model.predict(self.sample_tokens)
+
+        masked_index = np.where(self.sample_tokens == mask_token_id)
+        masked_index = masked_index[1]
+        mask_prediction = prediction[0][masked_index]
+
+        top_indices = mask_prediction[0].argsort()[-self.k :][::-1]
+        values = mask_prediction[0][top_indices]
+
+        for i in range(len(top_indices)):
+            p = top_indices[i]
+            v = values[i]
+            tokens = np.copy(sample_tokens[0])
+            tokens[masked_index[0]] = p
+            result = {
+                "input_text": self.decode(sample_tokens[0].numpy()),
+                "prediction": self.decode(tokens),
+                "probability": v,
+                "predicted mask token": self.convert_ids_to_tokens(p),
+            }
+            pprint(result)
+
