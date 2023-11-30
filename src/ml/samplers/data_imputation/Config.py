597abb5777e0dc78435040a5f0255ee4f2d1227d
YannickDetrois <yannick.detrois@outlook.de>
2023-11-30 18:35:48 +0100

    dataclasses cannot work

--- src/ml/samplers/data_imputation/Config.py
+++ src/ml/samplers/data_imputation/Config.py
@@ -1,25 +1,23 @@
-from dataclasses import dataclass
-
-@dataclass
-class ConfigVectorisation:
-    NUM_STATES: int = 4
-    NUM_ACTIONS: int = 6
-    TOKEN_DICT: dict = {'[PAD]': 0, '[MASK]': 1, '[SEP]': 2}
-    SEP_IDX: int = 8
-    SEP_LENGTH: int = 15  # Hyperparameter
-
-@dataclass
-class ConfigBert:
-    EMBED_DIM: int = 128
-    NUM_HEAD: int = 8
-    FF_DIM: int = 128
-    NUM_LAYERS: int = 1
-    LR: float = 0.001
-
-@dataclass
 class Config:
-    MAX_LEN: int = 256
-    BATCH_SIZE: int = 32  
-    VOCAB_SIZE: int = 30000 
-    vectorisation: ConfigVectorisation = ConfigVectorisation()
-    bert: ConfigBert = ConfigBert()
+    def __init__(self, MAX_LEN=256, BATCH_SIZE=32, VOCAB_SIZE=30000):
+        self.MAX_LEN = MAX_LEN
+        self.BATCH_SIZE = BATCH_SIZE
+        self.VOCAB_SIZE = VOCAB_SIZE
+        self.vectorisation = self.ConfigVectorisation()
+        self.bert = self.ConfigBert()
+
+    class ConfigVectorisation:
+        def __init__(self, NUM_STATES=4, NUM_ACTIONS=6, SEP_IDX=8, SEP_LENGTH=15):
+            self.NUM_STATES = NUM_STATES
+            self.NUM_ACTIONS = NUM_ACTIONS
+            self.TOKEN_DICT = {'[PAD]': 0, '[MASK]': 1, '[SEP]': 2}
+            self.SEP_IDX = SEP_IDX
+            self.SEP_LENGTH = SEP_LENGTH
+
+    class ConfigBert:
+        def __init__(self, EMBED_DIM=128, NUM_HEAD=8, FF_DIM=128, NUM_LAYERS=1, LR=0.001):
+            self.EMBED_DIM = EMBED_DIM
+            self.NUM_HEAD = NUM_HEAD
+            self.FF_DIM = FF_DIM
+            self.NUM_LAYERS = NUM_LAYERS
+            self.LR = LR
