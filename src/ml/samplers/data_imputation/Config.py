266659ebbe07ef27cb5f0e8a336fa417cc4bac27
AfroDeivid <92383275+AfroDeivid@users.noreply.github.com>
2023-11-30 17:49:57 +0100

    improve config

--- src/ml/samplers/data_imputation/Config.py
+++ src/ml/samplers/data_imputation/Config.py
@@ -1,16 +1,25 @@
 from dataclasses import dataclass
 
 @dataclass
+class Vectorization_info:
+    NUM_STATES: int = 4
+    NUM_ACTIONS: int = 6
+    TOKEN_DICT: dict = {'[PAD]': 0, '[MASK]': 1, '[SEP]': 2}
+    SEP_IDX: int = 8
+    SEP_LENGTH: int = 15  # Hyperparameter
+
+@dataclass
+class Bert_info:
+    EMBED_DIM: int = 128
+    NUM_HEAD: int = 8
+    FF_DIM: int = 128
+    NUM_LAYERS: int = 1
+    LR: float = 0.001
+
+@dataclass
 class Config:
-    NUM_STATES = 4
-    NUM_ACTIONS = 6
-    TOKEN_DICT={'[PAD]': 0, '[MASK]': 1, '[SEP]': 2}
-    SEP = 15
-    MAX_LEN = 256
-    BATCH_SIZE = 32
-    LR = 0.001
-    VOCAB_SIZE = 30000
-    EMBED_DIM = 128
-    NUM_HEAD = 8  
-    FF_DIM = 128 
-    NUM_LAYERS = 1
+    MAX_LEN: int = 256
+    BATCH_SIZE: int = 32  
+    VOCAB_SIZE: int = 30000 
+    Vectorization_info: Vectorization_info = Vectorization_info()
+    Bert_info: Bert_info = Bert_info()
