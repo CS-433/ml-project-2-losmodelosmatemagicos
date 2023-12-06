2e2ee1477c4be2baf54f35686ca5dc4c13cb61b3
AfroDeivid <92383275+AfroDeivid@users.noreply.github.com>
2023-12-06 22:51:10 +0100

    bert what did you did...

--- src/ml/samplers/data_imputation/MaskedTextGenerator.py
+++ src/ml/samplers/data_imputation/MaskedTextGenerator.py
@@ -13,10 +13,11 @@ class MaskedTextGenerator(keras.callbacks.Callback):
         top_k (int, optional): The number of top predictions to print for each mask. Defaults to 3.
     """
 
-    def __init__(self, sample_tokens, mask_token_id, n_masks=5, top_k=3):
-        self.n_masks = n_masks
+    def __init__(self, sample_tokens, mask_token_id,n_masks=5, top_k=3):
+
         self.sample_tokens = sample_tokens
         self.mask_token_id = mask_token_id
+        self.n_masks = n_masks
         self.top_k = top_k
 
     def on_epoch_end(self, epoch, logs=None):
@@ -30,16 +31,16 @@ class MaskedTextGenerator(keras.callbacks.Callback):
             logs (dict, optional): Dictionary of logs containing the training metrics. Defaults to None.
         """
         prediction = self.model.predict(self.sample_tokens)
-        print('prediction shape:', prediction.shape)
+        #print('prediction shape:', prediction.shape)
         
         masked_index = np.where(self.sample_tokens[0] == self.mask_token_id) # find where the first student sequence was masked
         mask_prediction = prediction[0][masked_index] # get the predictions for the masked tokens shape=(#masked_inputs, VOCAB_SIZE)
-        print('mask pred shape:', mask_prediction.shape)
+        #print('mask pred shape:', mask_prediction.shape)
         
         # getting the top_k predictions for the n_maks first masked predictions best_results.shape=(n_masks, top_k)
         best_results = mask_prediction[:self.n_masks].argsort()
         best_results = best_results[:, -self.top_k:][:, ::-1]
-        print('best results shape:', best_results.shape)
+        #print('best results shape:', best_results.shape)
         best_results_probs = mask_prediction[np.arange(self.n_masks).reshape(-1, 1), best_results] # getting the probabilities for the 5 first masked predictions
 
         '''print("\ninput_seq\n", self.sample_tokens[0])
