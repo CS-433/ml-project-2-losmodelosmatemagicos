9e274b7c06c80b94982fdc0210bda8390c714b5f
AfroDeivid <92383275+AfroDeivid@users.noreply.github.com>
2023-12-21 15:21:59 +0100

    Update data_loader.py

--- src/features/data_loader.py
+++ src/features/data_loader.py
@@ -19,7 +19,7 @@ class DataLoader:
         """
         ################################ CHEMLAB ################################
         ##### Features #####
-        path = '../data/ml4science_data.pkl'
+        path = '../data/ml4science_data_fake.pkl'
         path = PurePath(path)
         with open(path, 'rb') as fp:
             full_data = pickle.load(fp)
